0e85027fc19c34e79bd1e7c97f36bfe714743290
Maximilian Stolzle <maximilian@stoelzle.ch>
2024-03-08 11:17:34 +0100

    Add integration test

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -5,5 +5,13 @@ def add(a, b):
     return a + b
 
 
-def test_add():
+def test_add(unit_test_mocks: None):
     assert add(2, 3) == 5
+
+
+def test_int_hello():
+    """
+    This test is marked implicitly as an integration test because the name contains "_init_"
+    https://docs.pytest.org/en/6.2.x/example/markers.html#automatically-adding-markers-based-on-test-names
+    """
+    test_add()
